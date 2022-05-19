ccc5dc43cf6972726faf04f0dd2a7c97c0ec201a
Josh Jimenez <jjl1089@gmail.com>
2022-05-18 22:03:25 -0600

    Iniciando el Inicializador
    Iniciando el Inicializador

--- Inicializador.c
+++ Inicializador.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /*
 Programa Inicializador: Este programa se encarga de crear el ambiente. Pide los recursos y los inicializa de manera
 que los actores encuentren todo listo en el momento de empezar a funcionar. Este programa pide la cantidad de
@@ -9,6 +10,13 @@ debe morir después de realizar la inicialización.
 */
 
 int main() {
-	
 
+	//Inicializa los recursos
+	printf("\n------\n");
+	printf("Cuantas paginas o espacios en memoria?\n");
+	printf(">> ");
+	int cantPagsSegs;
+	scanf("%d", &cantPagsSegs);
+	printf("%i \n", cantPagsSegs);
+	//SolicitarMemoriaCompartida()
 }
