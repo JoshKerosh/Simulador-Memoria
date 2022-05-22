ff0d7b48584a9ed1e696354d36d11a9ec112a88e
Josh Jimenez <jjl1089@gmail.com>
2022-05-22 15:19:10 -0600

    Inicializador v1

--- Inicializador.c
+++ Inicializador.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 /*
@@ -8,6 +9,15 @@ que los actores encuentren todo listo en el momento de empezar a funcionar. Este
 páginas o espacios de memoria que va a haber y solicita la memoria compartida al sistema operativo. Este proceso
 debe morir después de realizar la inicialización. 
 */
+struct espacioMemoria{
+	bool libre;
+	int PID;
+};
+
+int SolicitarMemoria();
+int CrearMemoria();
+int CrearSemaforo();
+int CrearListasProcesos();
 
 int main() {
 
@@ -18,5 +28,34 @@ int main() {
 	int cantPagsSegs;
 	scanf("%d", &cantPagsSegs);
 	printf("%i \n", cantPagsSegs);
-	//SolicitarMemoriaCompartida()
+	SolicitarMemoria(cantPagsSegs);
+	printf("\nINICIALIZADOR TERMINADO!\n\n");
+
+}
+
+int SolicitarMemoria(int cant){
+	CrearMemoria(cant);
+	CrearSemaforo();
+	CrearListasProcesos();
+	return 0;
+}
+
+int CrearSemaforo(){
+	//inicia en verde
+	printf("Semáforo creado\n");
+	return 0;
+}
+
+int CrearMemoria(int cant){
+	// lista de espaciosMemoria, [cant]
+	printf("Memoria creada, %i espacios\n", cant);
+	return cant;
+}
+
+int CrearListasProcesos(){
+	//lista Ready
+	//lista Terminados
+	//lista Muertos
+	printf("Listas creadas\n");
+	return 0;
 }
